44638ab3e40eacc13f29705a96c5c8e6785e24c8
Lucas <lucas.edgerly@gmail.com>
2022-10-17 14:21:17 -0400

    estruturando css header components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-    <h1 className={styles.title}>Hello <span>WOrld</span></h1>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello WOrld
+      </h1>
     </>
   )
 }
